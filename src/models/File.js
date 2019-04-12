c0837a20b98d6f080777372f508427cbb3b51467
marcosfpinheiro <pinheiro.tech@gmail.com>
2019-04-11 23:58:12 -0300

    estrutura backend finalizada

---
+++ src/models/File.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+
+const File = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            require: true
+        },
+        path: {
+            type: String,
+            require: true
+        },
+    },
+    {
+        timestamps: true,
+        toObject: {virtuals: true},
+        toJSON: {virtuals: true}
+    }
+);
+
+File.virtual('url').get(function(){
+    const url = process.env.URL || 'http://localhost:3333'
+    return `${url}/files/${encodeURIComponent(this.path)}`;
+
+})
+
+module.exports = mongoose.model("File", File);
